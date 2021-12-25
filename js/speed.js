51a328e7ce7cb43e3474aa6f7c36f2ba6ca3e6e0
usernameusername2021 <itmolabs2021@gmail.com>
2021-12-25 21:25:38 +0300

    Update speed.js

--- js/speed.js
+++ js/speed.js
@@ -2,5 +2,5 @@ var startTime = new Date().getTime();
 
 window.onload = function () {
     var loadtime = new Date().getTime() - startTime;
-    alert("Вреемя загрузки страницы " + loadtime + " ms");
+    console.log(loadtime)
 }
